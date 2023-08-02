3dca281ef161b3bcb8483f87215f09b78234860c
Тимур Халилов <kotanonum@mail.ru>
2023-08-02 21:55:27 +0300

    комит

---
+++ src/main/java/ru/home/CreateHome.java
@@ -0,0 +1,16 @@
+package ru.home;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CreateHome implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!(sender instanceof Player)) return true;
+
+        return true;
+    }
+}
